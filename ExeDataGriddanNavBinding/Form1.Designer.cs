4ec9a885f8056896f0598353ab3dca6426c2e735
ERDA WYNE <erdawynee1@gmail.com>
2022-05-26 17:51:15 +0700

    Mengubah icon

--- ExeDataGriddanNavBinding/Form1.Designer.cs
+++ ExeDataGriddanNavBinding/Form1.Designer.cs
@@ -49,15 +49,16 @@ namespace ExeDataGriddanNavBinding
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Stencil", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label1.ForeColor = System.Drawing.Color.PaleVioletRed;
             this.label1.Location = new System.Drawing.Point(172, 230);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(123, 40);
+            this.label1.Size = new System.Drawing.Size(119, 39);
             this.label1.TabIndex = 1;
             this.label1.Text = "LOG IN";
             // 
             // pictureBox2
             // 
+            this.pictureBox2.BackColor = System.Drawing.Color.White;
             this.pictureBox2.Image = global::ExeDataGriddanNavBinding.Properties.Resources.login;
             this.pictureBox2.Location = new System.Drawing.Point(57, 279);
             this.pictureBox2.Name = "pictureBox2";
@@ -68,7 +69,7 @@ namespace ExeDataGriddanNavBinding
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Image = global::ExeDataGriddanNavBinding.Properties.Resources.photo1650371786;
+            this.pictureBox1.Image = global::ExeDataGriddanNavBinding.Properties.Resources.jangkar;
             this.pictureBox1.Location = new System.Drawing.Point(146, 30);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(149, 177);
@@ -113,21 +114,22 @@ namespace ExeDataGriddanNavBinding
             // 
             // button1
             // 
+            this.button1.BackColor = System.Drawing.Color.Pink;
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.ForeColor = System.Drawing.Color.Blue;
+            this.button1.ForeColor = System.Drawing.Color.PaleVioletRed;
             this.button1.Location = new System.Drawing.Point(57, 417);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(338, 27);
             this.button1.TabIndex = 6;
             this.button1.Text = "LOGIN";
-            this.button1.UseVisualStyleBackColor = true;
+            this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.BackColor = System.Drawing.Color.Transparent;
-            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.label2.ForeColor = System.Drawing.Color.PaleVioletRed;
             this.label2.Location = new System.Drawing.Point(213, 458);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(30, 17);
@@ -170,7 +172,7 @@ namespace ExeDataGriddanNavBinding
             this.Controls.Add(this.label1);
             this.Controls.Add(this.pictureBox1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Login";
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
